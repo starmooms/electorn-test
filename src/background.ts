c163f40433965414d75da63961805be1ee1dedbc
Guoxinming <1641092084@qq.com>
2020-07-21 01:04:34 +0800

    fix

--- src/background.ts
+++ src/background.ts
@@ -2,7 +2,7 @@
 
 import { app, protocol, BrowserWindow } from "electron";
 import { createProtocol } from "vue-cli-plugin-electron-builder/lib";
-import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
+// import installExtension, { VUEJS_DEVTOOLS } from "electron-devtools-installer";
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // Keep a global reference of the window object, if you don't, the window will
--- src/main/Update.ts
+++ src/main/Update.ts
@@ -1,8 +1,8 @@
-import { BrowserWindow } from "electron"
+// import { BrowserWindow } from "electron";
 
-class Update {
-  private win: BrowserWindow
-  constructor(win: BrowserWindow) {
-    this.win = win
-  }
-}
+// class Update {
+//   private win: BrowserWindow;
+//   constructor(win: BrowserWindow) {
+//     this.win = win;
+//   }
+// }

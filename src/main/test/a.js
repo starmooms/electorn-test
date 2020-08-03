7e296d857e0744b366302ab2105353ea3f1140d5
Gxming <1641092084@qq.com>
2020-08-03 08:40:13 +0800

    s

--- src/main/test/a.js
+++ src/main/test/a.js
@@ -1,4 +1,4 @@
-const crc16_ty = require('./crc16')
+// const crc16_ty = require('./crc16')
 
 // //10进制转16进制
 // var x = 99999999;
@@ -25,18 +25,14 @@ function PrefixZero(num, n) {
 
 const data = 'hellowhellow'
 const dataBuf = Buffer.from(data, 'ascii')
-console.log(dataBuf, '数据域')
+// console.log(dataBuf, '数据域')
 
 let dataBufLength = dataBuf.length.toString(16)
 dataBufLength = PrefixZero(dataBufLength, 4)
 const dataLenBuf = Buffer.alloc(2)
 dataLenBuf.writeUIntBE('0x' + dataBufLength, 0, 2)
 // console.log(dataBufLength, "长度")
-console.log(dataLenBuf, '数据域长度')
-
-const buf4 = Buffer.from([0x68, 0x03, 0x01, 0x00, 0x00, 0x68, 0xe1, 0x00])
-const buf5 = Buffer.concat([buf4, dataBuf, dataLenBuf])
-console.log(buf4, buf4.length)
+// console.log(dataLenBuf, '数据域长度')
 
 // function crc16(buf) {
 //   var crc = 0xFFFF;
@@ -55,9 +51,70 @@ console.log(buf4, buf4.length)
 // var crc = crc16(buf5);
 // console.log(crc.toString(16).toUpperCase(), "crc16");
 
-console.log(crc16_ty(buf5).toString(16), 'crc16')
+function crc16(buf) {
+  let crc = 0xffff
+  for (let i = 0; i < buf.length; i++) {
+    crc = crc ^ buf[i]
+    for (let j = 0; j < 8; j++) {
+      const temp = crc & 0x0001
+      crc >>= 1
+      if (temp) {
+        crc ^= 0xa001
+      }
+    }
+  }
+  return crc & 0xff
+}
 
-// https://www.yuque.com/u203312/vdb7mr/cqicrx
-// https://github.com/donvercety/node-crc16
-console.log(Buffer.from([0x12]))
-console.log(Buffer.from('0x12'))
+const buf4 = Buffer.from([0x68, 0x03, 0x01, 0xff, 0xff, 0x68, 0xe1, 0x00])
+const buf5 = Buffer.concat([buf4, dataLenBuf, dataBuf])
+// console.log('buf5', buf5)
+// console.log(crc16_ty(buf5).toString(16), 'crc16')
+const crc16Buf = Buffer.alloc(2)
+crc16Buf.writeUIntBE(`0x${crc16(buf5).toString(16)}`, 0, 2)
+const buf6 = Buffer.concat([buf5, crc16Buf, Buffer.from([0x16])])
+console.log('包内容', buf6)
+
+function getResult(s, l = 2) {
+  return `${PrefixZero(s.toString(16), l)}`
+}
+
+// function getArrResult(arr) {
+//   let s = ''
+//   arr.forEach(i => {
+//     s += getResult(i)
+//   })
+//   return s
+// }
+
+function getBufResults(b) {
+  let s = ''
+  for (let i = 0; i < b.length; i++) {
+    s += ` ${getResult(b[i])}`
+  }
+  return s
+}
+
+function showDetails(result) {
+  console.log('帧起始符：', getResult(result[0]))
+  console.log('从机类型：', getResult(result[1]))
+  console.log('版本号：', getResult(result[2]))
+  console.log('地址 主控：', getResult(result[3]))
+  console.log('地址 从控：', getResult(result[4]))
+  console.log('帧起始符：', getResult(result[5]))
+  console.log('控制码：', getResult(result[6]))
+  console.log('错误码：', getResult(result[7]))
+  console.log('数据域长度', getBufResults([result[8], result[9]]))
+  // console.log(parseInt(`${result[8]}${result[9]}`, 10))
+  const n = parseInt(`${result[8]}${result[9]}`, 10)
+  const sIndex = 10 + n
+  const data = result.slice(10, sIndex)
+  console.log('数据域：', getBufResults(data))
+  console.log('校验码：', getBufResults([result[sIndex], result[sIndex + 1]]))
+  console.log('帧结束符：', getResult(result[sIndex + 2]))
+}
+showDetails(buf6)
+// // https://www.yuque.com/u203312/vdb7mr/cqicrx
+// // https://github.com/donvercety/node-crc16
+// console.log(Buffer.from([0x12]))
+// console.log(Buffer.from('0x12'))

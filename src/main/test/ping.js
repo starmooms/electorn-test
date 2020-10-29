533fd31a83b81810430e6973b3a162d13cbf8cfd
GXM <1641092084@qq.com>
2020-10-29 18:37:04 +0800

    fix

---
+++ src/main/test/ping.js
@@ -0,0 +1,9 @@
+// const ping = require('ping')
+
+// const hosts = ['192.168.0.201', '192.168.0.93']
+
+// hosts.forEach(function(host) {
+//   ping.promise.probe(host).then(function(res) {
+//     console.log(res)
+//   })
+// })
